12b495376942614327e8804f588251c9329dd770
Yohannes Fantahun <88859176+Yohannes-F21@users.noreply.github.com>
2024-12-24 21:43:27 +0300

    setup-react-query and render-the-images-manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Gallery from "./Gallery";
+// import { ToastContainer } from "react-toastify";
 import SearchForm from "./SearchForm";
 import ThemeToggle from "./ThemeToggle";
 
 const App = () => {
   return (
-    <main className="max-w-screen-xl mx-auto">
-      <ThemeToggle />
-      <SearchForm />
-      {/* <Gallery />  */}
-    </main>
+    <>
+      {/* <ToastContainer position="top-right" /> */}
+      <main className="max-w-screen-xl mx-auto">
+        <ThemeToggle />
+        <SearchForm />
+        <Gallery />
+      </main>
+    </>
   );
 };
 export default App;
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "./utils";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { isLoading, data, error, isError } = useQuery({
+    queryKey: ["unsplash-images"],
+    queryFn: () => customFetch.get("/"),
+  });
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div
+          className="  inline-block h-14 w-14 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] text-[#645CFF] "
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 p-4 w-3/4 mx-auto">
+        {data?.data?.results.map((image) => {
+          return (
+            <div key={image.id} className="img">
+              <img src={image.urls.raw} alt={image.alt_description} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
